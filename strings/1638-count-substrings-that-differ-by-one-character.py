5a85e262957c8888380f73dab927532f20a46436
Aidar Siraev <asiraev@avtodoria.ru>
2025-08-30 15:39:51 +0300

    added 1638 bruteforce solution

--- strings/1638-count-substrings-that-differ-by-one-character.py
+++ strings/1638-count-substrings-that-differ-by-one-character.py
@@ -7,24 +7,52 @@ Return the number of substrings that satisfy the condition above.
 
 A substring is a contiguous sequence of characters within a string.
 
-
 link: https://leetcode.com/problems/count-substrings-that-differ-by-one-character
+
+O(n^3) solution
+
 """
 
+
 class Solution:
-    def countSubstrings(s: string, t: string) -> int:
+    def countSubstrings(self, s: str, t: str) -> int:
         out = 0
-        for i in range(len(s)):
+        i_size = len(s)
+        j_size = len(t)
+        w_size = max(len(s), len(t))
+
+        for w in range(1, w_size + 1):
+            for i in range(i_size):
+                if i + w > i_size:
+                    continue
+                for j in range(j_size):
+                    if j + w > j_size:
+                        continue
+                    out += self.compareSubstrings(s[i : i + w], t[j : j + w])
 
         return out
 
+    def compareSubstrings(self, x, y):
+        is_equal = 0
+
+        if len(x) != len(y):
+            return is_equal
+
+        for i in range(len(x)):
+            if x[i] != y[i]:
+                is_equal += 1
+
+        return is_equal == 1
+
 
 def main():
-    s = 'aba'
-    t = 'bba'
+    s = "ab"
+    t = "bb"
+    # s = 'computer'
+    # t = 'computation'
     sol = Solution()
-    return sol.countSubstrings(s, t)
+    print(sol.countSubstrings(s, t))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
